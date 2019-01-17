9164c3cb63aa99b3c6c9ea4e4006ca3c2ee03f79
root <root@DESKTOP-7TOTAEN.localdomain>
2019-01-17 00:56:06 -0800

    adds fully tested and functioning molecule.py

--- molecule.py
+++ molecule.py
@@ -9,22 +9,23 @@ class atom():
 	x = 0
 	y = 0
 	z = 0
-	atomicNumber = 1
+	Z = 1
 	N = 1	
 
 	#constructor fucntion for the atom class
 	#coord, list of coordinates in [x,y,z] format
 	#atomicNumber, integer that represents atomic number of the element, from 1 to infinity
 	#by default creates a hydrogen atom at the (0,0,0)
-	def __init__(coord, atomicNumberArg=1, N=1):
+	def __init__(self, coord=[0,0,0], Z=1, N=1):
 		
 		#assign position to coordinates		
-		x = coord[0]
-		y = coord[1]
-		z = coord[2]
+		self.x = coord[0]
+		self.y = coord[1]
+		self.z = coord[2]
 
-		#assign atomic number
-		atomicNumber = atomicNumberArg
+		#assign atomic number, and electron number
+		self.Z = Z
+		self.N = N
 
 #################################################################################
 
@@ -39,7 +40,7 @@ class molecule():
 	#constructor function for the molecule class
 	#atoms, can be either list of Chemical Symbols as strings to represent atoms, or list of actual atom objects to add to the molecule
 	#coordinates are only used if Chemical symbols supplied to the atoms list, and represent coordinates of the atoms 
-	def __init__(atoms=[], coordinates=[]):
+	def __init__(self, atoms=[], coordinates=[]):
 
 		stringAtomCounter = 0
 
@@ -48,21 +49,22 @@ class molecule():
 			
 			#if string atom name was passed in, then add atom in as string
 			if(type(atom) == str):
-				addStringAtom(atom, coordinates[stringAtomCounter])
+				self.addStringAtom(atom, coordinates[stringAtomCounter])
 				stringAtomCounter += 1
 			#else, add in atom as an atom object
 			else:
-				addAtom(atom)
+				self.addAtom(atom)
 
 #--------------------------------------------------------------------------------
 	#All class functions defined here
 
-	def addStringAtom():
+	def addStringAtom(self):
 		#atomData.append(atom(coord, atomStringToNumber()))
-		pass 	
+		print("pass")	
 
 #--------------------------------------------------------------------------------
 
-	def addAtom(atom):
-		atomData.append(atom)		
+	def addAtom(self, atom):
+		self.atomData.append(atom)		
+		self.N += atom.N
 
